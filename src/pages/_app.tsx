544c486e4173e5ab354aa82755e33526cfe9898f
robMolloy <robmolloy@hotmail.co.uk>
2024-06-10 13:23:47 +0100

    tailwind and basic layout setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Layout } from "@/modules/layout";
+import { useThemes } from "@/modules/themeSelector";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useThemes();
+  return (
+    <>
+      <Head>
+        <title>Pipeline Genius</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
