5080ca0c7b49ebfa9bb48c7d6c4cf64c19a874e2
robMolloy <robmolloy@hotmail.co.uk>
2024-06-11 17:50:20 +0100

    firestore get utils

---
+++ src/utils/firestoreUtils.ts
@@ -0,0 +1,35 @@
+import { db } from "@/firebase-config";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { ZodType, z } from "zod";
+
+export async function getSafeDocFromFirestore<T extends ZodType<any, any, any>>(p: {
+  id: string;
+  collectionName: string;
+  schema: T;
+}) {
+  const docRef = doc(db, p.collectionName, p.id);
+  const docSnap = await getDoc(docRef);
+
+  if (!docSnap.exists())
+    return {
+      success: false,
+      error: { message: `doc from ${p.collectionName} collection with id "${p.id}" not found` },
+    } as const;
+
+  const parseResponse = p.schema.safeParse({ id: p.id, ...docSnap.data() });
+  return parseResponse;
+}
+export async function getAllSafeDocsFromFirestore<T extends ZodType<any, any, any>>(p: {
+  collectionName: string;
+  schema: T;
+}) {
+  const querySnapshot = await getDocs(collection(db, p.collectionName));
+  const documents = querySnapshot.docs
+    .map((doc) => ({ id: doc.id, ...doc.data() }))
+    .map((doc) => {
+      const parseResponse = p.schema.safeParse(doc);
+      if (parseResponse.success) return parseResponse.data;
+    }) as z.infer<typeof p.schema>[];
+
+  return documents;
+}
